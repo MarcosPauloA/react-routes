4f74ea54e4339bcdf59c1e720bd9c56e3f1e40f1
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-09-24 21:33:49 -0300

    Added basic CSS and separated header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -9,13 +10,7 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
